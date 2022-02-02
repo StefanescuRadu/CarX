4875f11ddf885b8febc79b0ab5c9c98c924940ef
Stefanescu Radu <radu98fm@gmail.com>
2022-02-02 02:27:56 +0200

    added lorem ipsum text on main page

--- frontend/src/Components/LandingPage/Landing.tsx
+++ frontend/src/Components/LandingPage/Landing.tsx
@@ -11,9 +11,18 @@ const Landing = () =>{
                 <img className="object-fill w-[400px] " alt="highroad" src={require("./Porsche.jpg")}/>
             </div>
 
-            <h1 className="text-[90px] font-bold ">
-                Hello world!
-            </h1>
+            <div className=" font-audio absolute left-10 top-[100px] w-[600px]">
+                <h1 className="text-[90px] font-bold text-zinc-900">
+                    CarX
+                </h1>
+                <p className="break-all text-[30px] text-zinc-700 ">
+                    Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
+                    dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
+                    Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
+                    sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue.
+                </p>
+            </div>
+
         </div>
     )
 }
